ed08953053aed40e80c7eaec997baa93a3a1a389
someonespcl <madhavpoola4@gmail.com>
2024-09-04 16:24:06 +0530

    added firebase authentication

--- app/src/main/java/com/limitless/activities/RegisterActivity.java
+++ app/src/main/java/com/limitless/activities/RegisterActivity.java
@@ -18,8 +18,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.daimajia.androidanimations.library.Techniques;
 import com.daimajia.androidanimations.library.YoYo;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.limitless.R;
 import com.limitless.databinding.ActivityRegisterBinding;
 
